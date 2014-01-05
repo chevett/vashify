a32ee260f3eeedf5d3f0e55d8b2130ff372d2f72
Mike Chevett <mchevett@appnexus.com>
2014-01-05 18:03:37 -0500

    remove dead file

--- example/generate.js
+++
@@ -1,7 +0,0 @@
-var browserify = require('browserify');
-var b = browserify();
-
-b.transform('../index.js'); // or just 'vashify' in your case
-b.add('./entry.js');
-b.bundle().pipe(process.stdout);
-
